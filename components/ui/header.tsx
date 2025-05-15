2d14580bb4ab442aa996c745b85e0b69de9ee5b6
Dmitro <ox1995ter@gmail.com>
2025-05-15 21:06:25 +0300

    header

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -16,7 +16,7 @@ export default function Header() {
           <h1 className="text-2xl font-bold inline-flex items-center gap-2 text-foreground">
             <PawPrint className="w-6 h-6 text-primary" />
             {/* <PawPrint className="w-6 h-6 text-foreground" strokeWidth={2} /> */}
-            Поиск пород собак
+            Породы собак
           </h1>
           <h3 className="text-xl font-bold text-muted-foreground mt-1">
             Узнай всё о своём любимце 🐾
